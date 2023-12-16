e11894557d793acaaaebd366fa3e0c38c9687fce
KhoyrurRoykhan <khoirurroykan007@gmail.com>
2023-12-17 03:29:04 +0800

    update host

--- config/Database.js
+++ config/Database.js
@@ -5,7 +5,7 @@ const Database = new Sequelize({
     database: 'b2iobim6wv0db1b8vdtp',
     username: 'ualuujfll9mzkrde',
     password: 'R20jh72l4AxRrhCnuoxv',
-    host: 'b2iobim6wv0db1b8vdtp-mysql.services.clever-cloud.com-mysql.services.clever-cloud.com',
+    host: 'b2iobim6wv0db1b8vdtp-mysql.services.clever-cloud.com',
     dialect: 'mysql',
     dialectModule: mysql2,
 });
