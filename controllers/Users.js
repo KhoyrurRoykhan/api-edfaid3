3dd7e7e4a5abf3d3ae5b74cf27eecbad39471ace
KhoyrurRoykhan <khoirurroykan007@gmail.com>
2023-12-17 08:54:10 +0800

    auth login

--- controllers/Users.js
+++ controllers/Users.js
@@ -30,49 +30,38 @@ export const Register = async(req, res) => {
     }
 }
  
-export const Login = async (req, res) => {
+export const Login = async(req, res) => {
     try {
         const user = await Users.findAll({
-            where: {
-                email: req.body.email,
-            },
+            where:{
+                email: req.body.email
+            }
         });
-
         const match = await bcrypt.compare(req.body.password, user[0].password);
-
-        if (!match) return res.status(400).json({ msg: "Password Salah" });
-
+        if(!match) return res.status(400).json({msg: "Password Salah"});
         const userId = user[0].id;
         const name = user[0].name;
         const email = user[0].email;
-
-        const accessToken = jwt.sign(
-            { userId, name, email },
-            process.env.ACCESS_TOKEN_SECRET,
-            {
-                expiresIn: '25s',
-            }
-        );
-
-        const refreshToken = jwt.sign(
-            { userId, name, email },
-            process.env.REFRESH_TOKEN_SECRET,
-            {
-                expiresIn: '1d',
-            }
-        );
-
-        await Users.update({ refresh_token: refreshToken }, {
-            where: {
-                id: userId,
-            },
+        const accessToken = jwt.sign({userId, name, email}, process.env.ACCESS_TOKEN_SECRET,{
+            expiresIn: '25s'
         });
-
-        res.json({ accessToken, refreshToken });
+        const refreshToken = jwt.sign({userId, name, email}, process.env.REFRESH_TOKEN_SECRET,{
+            expiresIn: '1d'
+        });
+        await Users.update({refresh_token: refreshToken},{
+            where:{
+                id: userId
+            }
+        });
+        res.cookie('refreshToken', refreshToken,{
+            httpOnly: true,
+            maxAge: 24 * 60 * 60 * 1000
+        });
+        res.json({ accessToken });
     } catch (error) {
-        res.status(404).json({ msg: "Email tidak ditemukan" });
+        res.status(404).json({msg:"Email tidak ditemukan"});
     }
-};
+}
  
 export const Logout = async(req, res) => {
     const refreshToken = req.cookies.refreshToken;
