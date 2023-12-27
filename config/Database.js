f77ea8e2cf7627f0df96d52bd0e13d03ad106057
KhoyrurRoykhan <khoirurroykan007@gmail.com>
2023-12-27 14:19:59 +0800

    change db

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 import mysql2 from 'mysql2';
 
-const Database = new Sequelize('sql12670883','sql12670883','ZtgTwalVxq',{
-    host: 'sql12.freesqldatabase.com',
+const Database = new Sequelize('btfgnbuywd64cr2fp0qy','uvgfuxlyf5p7zgfq','7L8zCKiRTjbpRw5tStF1',{
+    host: 'btfgnbuywd64cr2fp0qy-mysql.services.clever-cloud.com',
     dialect: 'mysql',
     dialectModule: mysql2,
 });
